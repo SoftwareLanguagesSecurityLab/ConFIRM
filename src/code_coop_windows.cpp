5cbd5d788c895688122fcebdccb1045694116952
SoftwareLanguagesSecurityLab <50844992+SoftwareLanguagesSecurityLab@users.noreply.github.com>
2019-06-12 17:00:20 -0500

    Cite the OFI paper instead of giving a url.

--- src/code_coop_windows.cpp
+++ src/code_coop_windows.cpp
@@ -1,25 +1,40 @@
-/**************************************************************************************************
- * Copyright (c) 2019 Xiaoyang Xu, Masoud Ghaffarinia, Wenhao Wang, Kevin W. Hamlen, Zhiqiang Lin *
- *                                                                                                *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software  *
- * and associated documentation files (the "Software"), to deal in the Software without           * 
- * restriction, including without limitation the rights to use, copy, modify, merge, publish,     *
- * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the  *
- * Software is furnished to do so, subject to the following conditions:                           *
- *                                                                                                *
- * The above copyright notice and this permission notice shall be included in all copies or       *
- * substantial portions of the Software.                                                          *
- *                                                                                                *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING  *
- * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND     *
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,   *
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, *
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.        *
- **************************************************************************************************/
+/*************************************************************************************
+ * Copyright (c) 2019 Xiaoyang Xu, Masoud Ghaffarinia, Wenhao Wang, and Kevin Hamlen *
+ * The University of Texas at Dallas                                                 *
+ *                                                                                   *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of   * 
+ * this software and associated documentation files (the "Software"), to deal in     *
+ * the Software without restriction, including without limitation the rights to      *
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of  *
+ * the Software, and to permit persons to whom the Software is furnished to do so,   *
+ * subject to the following conditions:                                              *
+ *                                                                                   * 
+ * The above copyright notice and this permission notice shall be included in all    *
+ * copies or substantial portions of the Software.                                   *
+ *                                                                                   *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR        *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS  *
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR    *
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER    *
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN           *
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.        *
+ *************************************************************************************/
  
-// For a detailed explanation of CODE-COOP attack, see https://www.utdallas.edu/~kxh060100/wang17ccs.pdf
+/* This file is part of the ConFIRM test suite, whose initial documentation can be
+   found in the following publication:
 
-#include "helper.h"
+   Xiaoyang Xu, Masoud Ghaffarinia, Wenhao Wang, Kevin W. Hamlen, and Zhiqiang Lin.
+   "ConFIRM: Evaluating Compatibility and Relevance of Control-flow Integrity 
+   Protections for Modern Software."  In Proceedings of the 28th USENIX Security
+   Symposium, August 2019. */
+
+/* For a detailed explanation of CODE-COOP attack, see the following publication:
+
+   Wenhao Wang and Xiaoyang Xu and Kevin W. Hamlen.
+   "Object Flow Integrity."  In Proceedings of the 24th ACM Conference on Computer 
+   and Communications Security (CCS). */
+   
+#include "setup.h"
 #include "wininet.h"
 #include "winnls.h"
 #include "shobjidl.h"
@@ -37,9 +52,15 @@ int main()
 
     CoInitialize(NULL);
     
-    // CoCreateInstance() obtains an object reference (pad) of ActiveDesktop. And later QueryInterface() 
-    // obtains a new interface (puk) to ActiveDesktop.
-    hres = CoCreateInstance(CLSID_ActiveDesktop, NULL, CLSCTX_INPROC_SERVER, IID_IActiveDesktop, (LPVOID*)&pad);
+    // CoCreateInstance() obtains an object reference (pad) of ActiveDesktop. And 
+	// later QueryInterface() obtains a new interface (puk) to ActiveDesktop.
+    hres = CoCreateInstance(
+		CLSID_ActiveDesktop, 
+		NULL, 
+		CLSCTX_INPROC_SERVER, 
+		IID_IActiveDesktop, 
+		(LPVOID*)&pad
+	);
 
     if (SUCCEEDED(hres))
     {
@@ -48,10 +69,11 @@ int main()
         printf("Any input can lead to buffer overwrite: \n");
         scanf("%s", buffer);
 
-        // CFI solutions guarantee that the following function call targets a valid QueryInterface().
-        // Unfortunately, every implementation of QueryInterface() calls other virtual function(e.g. AddRef())
-        // by default. With the above data corruption vulnerability, an attacker can tailer counterfeit
-        // object and then redirect control flow to arbitrary code. 
+        // CFI solutions guarantee that the following function call targets a valid
+		// QueryInterface(). Unfortunately, every implementation of QueryInterface()
+		// calls other virtual functions (e.g. AddRef) by default. With the above
+		// data corruption vulnerability, an attacker can tailer counterfeit object
+		// and then redirect control flow to arbitrary code. 
         IPersistFile* puk;
         hres = pad-> QueryInterface(IID_IUnknown, (LPVOID*)&puk);
         if (SUCCEEDED(hres))
@@ -72,4 +94,3 @@ int main()
 
     return 0;
 }
-
