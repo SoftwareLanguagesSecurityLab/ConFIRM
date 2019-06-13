57619a3abd57b88f53ba46e394d1f11195080853
SoftwareLanguagesSecurityLab <50844992+SoftwareLanguagesSecurityLab@users.noreply.github.com>
2019-06-12 23:07:34 -0500

    license block modified

--- src/unmatched_pair.cpp
+++ src/unmatched_pair.cpp
@@ -1,23 +1,34 @@
-/**************************************************************************************************
- * Copyright (c) 2019 Xiaoyang Xu, Masoud Ghaffarinia, Wenhao Wang, Kevin W. Hamlen, Zhiqiang Lin *
- *                                                                                                *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software  *
- * and associated documentation files (the "Software"), to deal in the Software without           * 
- * restriction, including without limitation the rights to use, copy, modify, merge, publish,     *
- * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the  *
- * Software is furnished to do so, subject to the following conditions:                           *
- *                                                                                                *
- * The above copyright notice and this permission notice shall be included in all copies or       *
- * substantial portions of the Software.                                                          *
- *                                                                                                *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING  *
- * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND     *
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,   *
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, *
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.        *
- **************************************************************************************************/
+/*************************************************************************************
+ * Copyright (c) 2019 Xiaoyang Xu, Masoud Ghaffarinia, Wenhao Wang, and Kevin Hamlen *
+ * The University of Texas at Dallas                                                 *
+ *                                                                                   *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of   *
+ * this software and associated documentation files (the "Software"), to deal in     *
+ * the Software without restriction, including without limitation the rights to      *
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of  *
+ * the Software, and to permit persons to whom the Software is furnished to do so,   *
+ * subject to the following conditions:                                              *
+ *                                                                                   *
+ * The above copyright notice and this permission notice shall be included in all    *
+ * copies or substantial portions of the Software.                                   *
+ *                                                                                   *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR        *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS  *
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR    *
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER    *
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN           *
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.        *
+ *************************************************************************************/
+ 
+/* This file is part of the ConFIRM test suite, whose initial documentation can be
+   found in the following publication:
 
-#include "helper.h"
+   Xiaoyang Xu, Masoud Ghaffarinia, Wenhao Wang, Kevin W. Hamlen, and Zhiqiang Lin.
+   "ConFIRM: Evaluating Compatibility and Relevance of Control-flow Integrity 
+   Protections for Modern Software."  In Proceedings of the 28th USENIX Security
+   Symposium, August 2019. */
+
+#include "setup.h"
 #include <setjmp.h>
 
 void exception_test();
@@ -65,17 +76,18 @@ void exception_callee2()
 {
     printf("3. a message in exception_callee2\n");
     
-    // This exception being catched by the catch block in exception_test(), results in popping stack
-    // frames from multiple calls, followed by a return at the end of exception_test(). Shadow stack
-    // defenses that are implemented based on call/return matching may be problematic.
+    // This exception being caught by the catch block in exception_test(), results in
+    // popping stack frames from multiple calls, followed by a return at the end of
+    // exception_test(). Shadow stack defenses that are implemented based on
+    // call/return matching may be problematic.
     throw 7;
 }
 
 void longjmp_test()
 {
     printf("5. a message in longjmp_test\n");
-    // setjmp() returns 0 when called directly, so control flows to the else block. The if block is 
-    // executed as the jump target of longjmp().
+    // setjmp() returns 0 when called directly, so control flows to the else block.
+    // The if block is executed as the jump target of longjmp().
     if (setjmp(buf))
         printf("8. a message after longjmp\n");
     else
@@ -92,8 +104,8 @@ void longjmp_callee2()
 {
     printf("7. a message in longjmp_callee2\n");
     
-    // This longjmp to longjmp_test() leads to an unmatched call/return pair, followed by a return from 
-    // longjmp_test to main.
+    // This longjmp to longjmp_test() leads to an unmatched call/return pair,
+    // followed by a return from longjmp_test to main.
     longjmp(buf, 1);
     printf("-1, oops, should never reach this!\n");
 }
